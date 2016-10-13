853f04522d4b76311d092e7a8688eca531dbe8bf
Weijian Meng <meng.weijian1994@gmail.com>
2016-10-13 16:21:48 +1100

    plot color bar
    under the dendrogram according to given classification

--- cluster.r
+++ cluster.r
@@ -1,6 +1,12 @@
 # cluster analysis
-d <- as.dist(read.csv("/DIRECTORY/sin_weighted.csv"))
-hc = hclust(d, method = 'ward.D')
+d <- read.csv("DIRECTORY/sin_weighted.csv")
+dM <- as.dist(d)
+hc = hclust(dM, method = 'ward.D')
 
 # plot dendrogram
 plot(hc, cex = 0.4)
+
+# plot dendrogram with color bar underneath corresponding to external classification
+library(WGCNA)
+classlabels <- unlist(read.csv("DIRECTORY/worddictentries.csv", colClasses=c('NULL','NULL','NULL','NULL',NA,'NULL')))
+plotDendroAndColors(hc, labels2colors(classlabels), cex.dendroLabels=.4)
